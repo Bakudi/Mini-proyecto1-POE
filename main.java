6106f854c2de9b149b53c36ba2b6c037bda95962
JuanCamiloGutiV <144349089+JuanCamiloGutiV@users.noreply.github.com>
2023-10-02 11:18:52 -0500

    Add files via upload
    Implementación de la clase métodos, menú principal y cuarto/quinto punto

---
+++ main.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) throws Exception{
+    Byte num;
+    Metodos.menu(); 
+
+}
+}
