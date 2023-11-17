d6258e89bb3715a1e92c5b9d3e1df7773c487e0f
Andres Felipe Rojas <107456253+Bakudi@users.noreply.github.com>
2023-11-16 20:03:32 -0500

    Update Ciudad.java

--- Ciudad.java
+++ Ciudad.java
@@ -1,8 +1,7 @@
 public enum Ciudad {
-    ANDALUCIA,bugalagrande,elcerrito,ginebra,guacari,buga,riofrio,trujillo,calima,restrepo,sanpedro,tulua,yotoco,elaguila,
-    alcala,ansermanuevo,argelia,elcairo,launion,lavictoria,ulloa,bolivar,cartago,eldovio,obando,roldanillo,toro,versalles,
-    zarzal,buenaventura,caicedonia,sevilla,cali,candelaria,palmira,daguaflorida,jamundi,lacumbre,pradera,vijes,yumbo;
-
+    ANDALUCIA,BUGALAGRANDE,ELCERRITO,GINEBRA,GUACARI,BUGA,RIOFRIO,TRUJILLO,CALIMA,RESTREPO,SANPEDRO,TULUA,YOTOCO,ELAGUILA,
+ALCALA,ANSERMANUEVO,ARGELIA,ELCAIRO,LAUNION,LAVICTORIA,ULLOA,BOLIVAR,CARTAGO,ELDOVIO,OBANDO,ROLDANILLO,TORO,VERSALLES,
+ZARZAL,BUENAVENTURA,CAICEDONIA,SEVILLA,CALI,CANDELARIA,PALMIRA,DAGUAFLORIDA,JAMUNDI,LACUMBRE,PRADERA,VIJES,YUMBO;
     public static Ciudad obtenerCiudad(String nombreCiudad) {
         for (Ciudad ciudad : Ciudad.values()) {
             if (ciudad.name().equalsIgnoreCase(nombreCiudad)) {
