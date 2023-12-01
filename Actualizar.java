026dc1f87656c6744a836be1a0f6e0416c3c18f2
Andres Felipe Rojas <107456253+Bakudi@users.noreply.github.com>
2023-11-30 22:31:29 -0500

    Update Actualizar.java

--- Actualizar.java
+++ Actualizar.java
@@ -214,15 +214,4 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JPanel;
 
-public class Actualizar extends JPanel implements ActionListener {
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
-    }
-
-
-
-}
 
